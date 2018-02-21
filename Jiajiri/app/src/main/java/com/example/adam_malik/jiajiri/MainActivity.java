e27233202adc062adfdc863b5049ee7503ec526f
malik303 <a.malicky1@gmail.com>
2018-02-22 00:43:18 +0300

    Jiajiri project

---
+++ Jiajiri/app/src/main/java/com/example/adam_malik/jiajiri/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.adam_malik.jiajiri;
+
+import
+        android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements view.onClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
